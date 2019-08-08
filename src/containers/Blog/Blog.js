20a80ea16cb2bb65f3c6a322084e7d95f7bc1dc7
Shubham17 Gupta <shubham.sg2901@gmail.com>
2019-08-08 15:59:28 +0530

    Using react-router's Link component instead of anchor tags for routing

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Route} from 'react-router-dom';
+import {Route, Link} from 'react-router-dom';
 import FullPost from './FullPost/FullPost';
 import NewPost from './NewPost/NewPost';
 import styles from './Blog.module.css';
@@ -20,8 +20,8 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/new-post">New Post</a></li>
+                            <li><Link to="/" exact>Home</Link></li>
+                            <li><Link to="/new-post">New Post</Link></li>
                         </ul>
                     </nav>
                 </header>
