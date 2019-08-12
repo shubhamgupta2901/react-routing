4376c262f0962fbf415b42c3082718b3f898e541
Shubham17 Gupta <shubham.sg2901@gmail.com>
2019-08-12 16:57:09 +0530

    Redirecting requests

--- src/nested-routing/containers/Blog/Blog.js
+++ src/nested-routing/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Route, NavLink, Switch} from 'react-router-dom';
+import {Route, NavLink, Switch, Redirect} from 'react-router-dom';
 import NewPost from './NewPost/NewPost';
 import styles from './Blog.module.css';
 import Posts from './Posts/Posts';
@@ -19,7 +19,6 @@ class Blog extends Component {
                                 <NavLink 
                                     activeClassName={styles.activeNavLink}
                                     to="/posts/" 
-                                    exact
                                 >
                                     Posts
                                 </NavLink>
@@ -37,9 +36,17 @@ class Blog extends Component {
                 </header>
                 
                 {/* <Route path="/" exact render={()=><Posts postSelectedHandler= {this.postSelectedHandler}/>}/>*/}
+                 {/** While using the switch statment, this very first route that is matched is rendered, and it doesn't look any furthur.*/}
                 <Switch>
                     <Route path="/new-post" component={NewPost}/>
-                    <Route path="/posts"  component={Posts}/>    
+                    <Route path="/posts"  component={Posts}/>   
+                    {/** This statement is put for redirecting the user to Posts when he goes to home. 
+                        *This feature works fine and we can keep it like this without any issues, 
+                        * however it does not change the url to "http://localhost:3000/posts" when we go to "http://localhost:3000/" 
+                        * For this purpose we use react-router's Redirect**/}
+                    {/* <Route path="/"  component={Posts}/>    */}
+                    {/** Redirect takes two major props, 'from' : from which route?, and 'to' : redirect to which route?**/}
+                    <Redirect from="/" to={"/posts"}/>
                 </Switch>
                
             </div>
